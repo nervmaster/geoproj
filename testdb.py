688227d2efe376932db908baee52be4a20f0dbbc
Henrique P Borges <hpborges@gmail.com>
2016-08-24 15:18:58 -0300

    Feito mudancas no tesbd e testopencs
    testbd ja pega dados do bd e faz download das imagens
    
    testopencs esta sendo adaptado para cortar as imagens

--- testdb.py
+++ testdb.py
@@ -1,5 +1,27 @@
 import psycopg2
 import json
+import urllib
+from testopencs import crop_new_image
+
+# debug de testopencs
+images = list()
+images.append('./1.jpg')
+images.append('./2.jpg')
+images.append('./3.jpg')
+images.append('./4.jpg')
+images.append('./5.jpg')
+images.append('./6.jpg')
+images.append('./7.jpg')
+crop_new_image(images)
+
+exit(1)
+
+
+
+
+
+
+
 
 config = json.load(open('db.config'))
 
@@ -14,5 +36,58 @@ cur.execute("SELECT relname FROM pg_class WHERE relkind='r' AND relname !~ '^(pg
 # Todo o conteudo
 cur.execute('SELECT * FROM "Minerios"')
 rows = cur.fetchall()
+bd = list()
+
+# Pega todo o conteudo do BD
 for row in rows:
-	print row
+	minerio = dict()
+	minerio['id'] = row[0]
+	minerio['minerio'] = row[1]
+	minerio['chapas'] = list()
+
+	# Pegar as chapas
+	cur.execute('SELECT * FROM "Chapas" WHERE "minerio_cd"=' + str(minerio['id']) + ';')
+	chapas_rows = cur.fetchall()
+	
+	for chapa_row in chapas_rows:
+		chapa = dict()
+		chapa['id'] = chapa_row[0]
+		# Pegar outras informacoes da chapa se precisar
+		chapa['fotos'] = list()
+
+		# Pegar as fotos
+		cur.execute('SELECT * FROM "Fotos" WHERE "chapa_cd"=' + str(chapa['id']) + ';')
+		fotos_rows = cur.fetchall()
+
+		for foto_row in fotos_rows:
+			foto = dict()
+			foto['id'] = foto_row[0]
+			foto['chapa_id'] = foto_row[1]
+			foto['endereco'] = 'http://www.matheussilva.com/bdgeo/' + foto_row[2]
+			foto['angulo'] = foto_row[3]
+			foto['luz'] = foto_row[4]
+
+			chapa['fotos'].append(foto)
+
+		minerio['chapas'].append(chapa)
+	bd.append(minerio)
+
+# Bd propriamente no formato json
+# bd = json.dumps(bd)
+
+# Iterando pelo BD
+label_list = list()
+arg_list = list()
+
+for minerio in bd:
+	for chapa in minerio['chapas']:
+		images = list()
+		counter = 0
+		for foto in chapa['fotos']:
+			image_path = './' + str(counter) + '.jpg'
+			urllib.urlretrieve(foto['endereco'], image_path)
+			images.append(image_path)
+			counter = counter + 1
+		crop_new_image(images)
+			
+
--- testopencs.py
+++ testopencs.py
@@ -2,6 +2,8 @@ import argparse
 import cv2
 import numpy as np
 
+
+
 # Funcao de dist euclidiana
 def distance(p0, p1):
 	'Computa a disntacia euclidiana ao quadrado'
@@ -14,8 +16,96 @@ def nn_classify(training_set, training_labels, new_example):
 	return training_labels[nearest]
 
 
-refPt = []
-cropping = False
+
+def crop_new_image(images):
+	refPt = []
+	cropping = False
+	global image
+
+	for i in range(0, len(images)):
+		# Seta as chamadas de funcao dos eventos
+		image = cv2.imread(images[i])
+		clone = image.copy()
+		cv2.namedWindow("image", cv2.WINDOW_NORMAL)
+		cv2.setMouseCallback("image", click_and_crop)
+
+		# Motor da  janela
+		while True:
+			cv2.imshow("image", image)
+			key = cv2.waitKey(1) & 0xFF
+
+			if key == ord("r"):
+				image = clone.copy()
+
+			elif key == ord("c"):
+				break
+
+		# Pegou a imagem recortada
+		if len(refPt) == 2:
+			roi = clone[refPt[0][1]:refPt[1][1], refPt[0][0]:refPt[1][0]]
+			# ROI eh a imagem recortada
+
+			# Mudar espaco de cores
+			roi = np.float32(roi)
+			roi = roi / 255.0
+			roi = cv2.cvtColor(roi, cv2.COLOR_BGR2LAB)
+
+			# iterar os pixels
+			# cria um array em numpy
+			l,a,b = cv2.split(roi)
+			
+			# valor l
+			l = np.average(l, axis=0)
+			l = np.average(l, axis=0)
+
+			# valor a
+			a = np.average(a, axis=0)
+			a = np.average(a, axis=0)
+			
+			# valor b
+			b = np.average(b, axis=0)
+			b = np.average(b, axis=0)
+
+			print 'avg: ',l, a, b
+
+			# MIN
+			l,a,b = cv2.split(roi)
+
+			l = np.min(l, axis=0)
+			l = np.min(l, axis=0)
+
+			# valor a
+			a = np.min(a, axis=0)
+			a = np.min(a, axis=0)
+			
+			# valor b
+			b = np.min(b, axis=0)
+			b = np.min(b, axis=0)
+
+			print 'min: ',l, a, b
+
+			# MAX
+			l,a,b = cv2.split(roi)
+
+			l = np.max(l, axis=0)
+			l = np.max(l, axis=0)
+
+			# valor a
+			a = np.max(a, axis=0)
+			a = np.max(a, axis=0)
+			
+			# valor b
+			b = np.max(b, axis=0)
+			b = np.max(b, axis=0)
+
+			print 'max: ',l, a, b
+
+			roi = cv2.cvtColor(roi, cv2.COLOR_LAB2BGR)
+			cv2.imshow("ROI",roi)
+			cv2.waitKey(0)
+		cv2.destroyAllWindows() 
+
+
 #pega coordenadas do crop retangulo
 def click_and_crop(event, x, y, flags, param):
 	global refPt, cropping
@@ -30,90 +120,5 @@ def click_and_crop(event, x, y, flags, param):
 
 		cv2.rectangle(image, refPt[0], refPt[1], (0,255,0), 2)
 		cv2.imshow("image", image)
-# Pega a imagem
-ap = argparse.ArgumentParser()
-ap.add_argument("-i", "--image", required=True, help="Path to the image")
-args = vars(ap.parse_args())
 
-# Seta as chamadas de funcao dos eventos
-image = cv2.imread(args["image"])
-clone = image.copy()
-cv2.namedWindow("image")
-cv2.setMouseCallback("image", click_and_crop)
 
-# Motor da  janela
-while True:
-	cv2.imshow("image", image)
-	key = cv2.waitKey(1) & 0xFF
-
-	if key == ord("r"):
-		image = clone.copy()
-
-	elif key == ord("c"):
-		break
-		
-# Pegou a imagem recortada
-if len(refPt) == 2:
-	roi = clone[refPt[0][1]:refPt[1][1], refPt[0][0]:refPt[1][0]]
-	# ROI eh a imagem recortada
-
-	# Mudar espaco de cores
-	roi = np.float32(roi)
-	roi = roi / 255.0
-	roi = cv2.cvtColor(roi, cv2.COLOR_BGR2LAB)
-
-	# iterar os pixels
-	# cria um array em numpy
-	l,a,b = cv2.split(roi)
-	
-	# valor l
-	l = np.average(l, axis=0)
-	l = np.average(l, axis=0)
-
-	# valor a
-	a = np.average(a, axis=0)
-	a = np.average(a, axis=0)
-	
-	# valor b
-	b = np.average(b, axis=0)
-	b = np.average(b, axis=0)
-
-	print 'avg: ',l, a, b
-
-	# MIN
-	l,a,b = cv2.split(roi)
-
-	l = np.min(l, axis=0)
-	l = np.min(l, axis=0)
-
-	# valor a
-	a = np.min(a, axis=0)
-	a = np.min(a, axis=0)
-	
-	# valor b
-	b = np.min(b, axis=0)
-	b = np.min(b, axis=0)
-
-	print 'min: ',l, a, b
-
-	# MAX
-	l,a,b = cv2.split(roi)
-
-	l = np.max(l, axis=0)
-	l = np.max(l, axis=0)
-
-	# valor a
-	a = np.max(a, axis=0)
-	a = np.max(a, axis=0)
-	
-	# valor b
-	b = np.max(b, axis=0)
-	b = np.max(b, axis=0)
-
-	print 'max: ',l, a, b
-
-	roi = cv2.cvtColor(roi, cv2.COLOR_LAB2BGR)
-	cv2.imshow("ROI",roi)
-	cv2.waitKey(0)
-
-cv2.destroyAllWindows()
