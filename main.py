2ca3d4f41eced70ea38f3ad6635ee4b62d31ccf8
Henrique P Borges <hpborges@gmail.com>
2017-03-06 08:19:17 -0300

    Modificado o main para receber argumento por linha de comando

--- main.py
+++ main.py
@@ -5,52 +5,60 @@ from sklearn import neural_network,linear_model,svm,naive_bayes,neighbors, metri
 import numpy as np, scipy.stats as st
 import csv
 import itertools
-from ann import build_ann
+import sys
 from training import train
 from multiprocessing import Process, Lock, Queue, Pool, Manager
 from functools import partial
 
 param = ['xpl', 'pleoc', 'biref', 'ppl', 'tex', 'opa']
-# all_set, labels = iterate_alligholli_dataset(param = param, normalize = False, pairs = 1)
-# # data_conf = make_confidence_interval(all_set, labels)
-# make_csv(all_set, labels)
-# exit(1)
 
-with open('results.csv', 'w') as csvfile:
-	fieldnames = ['param','random','kNN','naive','linear','svm','sgdc','dtree','ann']
-	writer = csv.DictWriter(csvfile, fieldnames = fieldnames)
-	writer.writeheader()
+def makecsv():
+	all_set, labels = iterate_alligholli_dataset(param = param, normalize = False, pairs = 2)
+	data_conf = make_confidence_interval(all_set, labels)
+	make_csv(all_set, labels)
 
-	# Code block for setting ANN
-	#r = read_from_csv('data.csv', param)
-	#build_ann(r['entries'], r['labels'])
-	#print 'built'
-	#exit(1)
-
-	lock = Lock()
-	m = Manager()
-	linhas = m.Queue()
-	args = list()
-	workers = Pool(5)
-	for L in range(0, len(param) + 1):
-		if L < 1:
-			continue
-		pp = list()
-		for subset in itertools.combinations(param, L):
-			args.append(subset)
-
-	func = partial(train, linhas)
-	workers.map(func, args)
-	workers.close()
-	workers.join()
-
-	while not linhas.empty():
-		writer.writerow(linhas.get())
+def train():
+	with open('results.csv', 'w') as csvfile:
+		fieldnames = ['param','random','kNN','naive','linear','svm','sgdc','dtree','ann']
+		writer = csv.DictWriter(csvfile, fieldnames = fieldnames)
+		writer.writeheader()
 
 
-		# 	p = Process(target = train, args = (subset, linhas, lock))
-		# 	p.start()
-		# 	pp.append(p)
-		#
-		# for p in pp:
-		# 	p.join()
+		lock = Lock()
+		m = Manager()
+		linhas = m.Queue()
+		args = list()
+		workers = Pool(4)
+		for L in range(0, len(param) + 1):
+			if L < 1:
+				continue
+			pp = list()
+			for subset in itertools.combinations(param, L):
+				args.append(subset)
+
+			func = partial(train, linhas)
+			workers.map(func, args)
+			workers.close()
+			workers.join()
+
+		while not linhas.empty():
+			writer.writerow(linhas.get())
+
+
+def main(argv):
+	if len(argv) < 2:
+		print 'Argumentos insuficientes'
+		exit(1)
+	if argv[1] == 'makecsv':
+		print 'creating csv'
+		makecsv()
+	elif argv[1] == 'train':
+		print 'training data	'
+		train()
+
+	else:
+		'invalid arg'
+
+
+if __name__ == '__main__':
+	main(sys.argv)
