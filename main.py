9c3c460d07e89c3b05f117ec5864a5d0f43b2b02
Henrique Pereira Borges <hpborges@gmail.com>
2017-09-20 11:56:51 -0300

    Adicionado comentarios ao manin.py Removido arquivo testdb por não ser mais relevante

--- main.py
+++ main.py
@@ -11,15 +11,18 @@ from training import train, evaluate
 from multiprocessing import Process, Lock, Queue, Pool, Manager
 from functools import partial
 
-param = ['xpl', 'pleoc', 'biref', 'ppl', 'tex', 'opa']
+# Todos os parametros disponiveis para extração
+all_param = ['xpl', 'pleoc', 'biref', 'ppl', 'tex', 'opa']
+
+# Os parâmetros usados na última iteração
 singles = ['xpl', 'ppl', 'tex', 'opa']
+
+# Parâmetros para algum teste requisitado
 teste = ['xpl', 'ppl', 'tex', 'opa']
 
-
-def multi_run_wrapper(args):
-    return train(*args)
-
-
+# Função para criar um CSV com o CDMAS
+# Recebe como argumento O nome do arquivo csv para salvar
+# Chamada: makecsv("arquivo_de_saida.csv")
 def makecsv(argv):
     if len(argv) < 1:
         print 'missing <filename>'
@@ -29,13 +32,30 @@ def makecsv(argv):
     iterate_alligholli_dataset(arq, writer, param=param)
     arq.close()
 
+# Função para criar um CSV com CDGEO
+# Chamada: maketraincsv("arquivo_de_saida.csv")
+def maketraincsv(argv):
+    if len(argv) < 1:
+        print 'missing filepath to save to'
+        exit(1)
+    filename = argv[0]
+    arq, writer = make_csv(filename)
+    iterate_gathered_data(arq, writer, param)
+    arq.close()
 
+
+# Faz uma validação cruzada com o dataset CSV indicado como argumento
+# E salva os resultados em arquivo CSV
+# Chamada: cross_validation("arquivo_de_entrada.csv", "arquivo_de_saida.csv")
 def cross_validation(argv):
     if len(argv) < 2:
         print 'missing csv data file to read from and number of workers'
         exit(1)
+    
     filename = argv[0]
-    nworkers = int(argv[1]) if int(argv[1]) > 0 else 1
+    
+    # Caso queira que a validação cruzada seja em paralelo mudar para nworkes para o numero de cores desejados
+    nworkers = 1
 
     with open('results.csv', 'w') as csvfile:
         fieldnames = ['param', 'random', 'kNN', 'dtree']
@@ -50,7 +70,7 @@ def cross_validation(argv):
         for i in range(1, len(singles) + 1):
             for sub in itertools.combinations(singles, i):
                 args.append((linhas, sub, filename))
-            break
+            break #Remova essa linha caso queira todas as combinações possiveis de parâmetros
             
 
         mpr = [pool.apply_async(train, arg) for arg in args]
@@ -63,40 +83,9 @@ def cross_validation(argv):
             except:
                 writer.writerow(linhas.get())
         
-
-        
         print 'escrevendo'
         while not linhas.empty():
             writer.writerow(linhas.get())
-
-
-def maketraincsv(argv):
-    if len(argv) < 1:
-        print 'missing filepath to save to'
-        exit(1)
-    filename = argv[0]
-    arq, writer = make_csv(filename)
-    iterate_gathered_data(arq, writer, param)
-    arq.close()
-
-
-def debug(datafile, testfile):
-    # will change this whole function
-    with open('evaluate_results.csv', 'w') as csvfile:
-        fieldnames = ['param', 'kNN', 'dtree']
-        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-        writer.writeheader()
-        m = Manager()
-        linhas = m.Queue()
-
-        for i in range(1, len(singles) + 1):
-            for sub in itertools.combinations(singles, i):
-                evaluate(linhas, sub, datafile, testfile)
-
-        print 'escrevendo'
-        while not linhas.empty():
-            writer.writerow(linhas.get())
-
 
 def main(argv):
     if len(argv) < 1:
@@ -108,15 +97,11 @@ def main(argv):
     elif argv[1] == 'train':
         print 'training data'
         cross_validation(argv[2:])
-    elif argv[1] == 'debug':
-        print 'debug'
-        debug(argv[2:])
     elif argv[1] == 'maketraincsv':
         print 'make csv train'
         maketraincsv(argv[2:])
     else:
         'invalid arg'
-
 
 if __name__ == '__main__':
     main(sys.argv)
