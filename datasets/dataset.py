c3103bf32f1f305344d4920cc42f95a883bc6f33
Henrique Borges <nervmaster@localhost.localdomain>
2018-06-03 20:28:44 -0300

    Minor improvements on dataset
    Writing some code common to all datasets
    
    TODO:
    	Write CSV File
    	create first specific datasets

--- datasets/dataset.py
+++ datasets/dataset.py
@@ -1,10 +1,13 @@
 from abc import ABC, abstractmethod
 
 class Dataset(ABC):
-    self.__data = []
-    self.__label = []
+    self.__data 
+    self.__label 
+    self.__paramNames 
+    self.__csvFileName 
 
-    def __init__(self, csvFileName = False):
+    def __init__(self, paramNames, csvFileName = None):
+        # ParamNames must be passed as arg for the desired parameters
         # Checks if has a csvFileName
         # if not parse files
         # if yes load the csv
@@ -22,8 +25,44 @@ class Dataset(ABC):
     def parseFiles(self):
         pass
 
-    def writeCsv(self, csvFileName):
+    def __createCsvWriter(self, csvFileName, headerList):
+        if csvFileName is None:
+            csvFileName = self.__csvFileName
+        if headerList is None:
+            headerList = self.__paramNames
+        
+        try:
+            arq = open(csvFileName, 'w')
+        except Exception as e:
+            return e
+        
+        writer = csv.writer(arq, delimiter=';')
+        writer.writerow(header)
+        arq.flush()
+
+        return arq, writer
+
+    @abstractmethod
+    def writeCsv(self, csvFileName = None, headerList = None):
         pass
+        
     
-    def readCsv(self, csvFileName):
-        pass
+    def readCsv(self, labelColumnName, csvFileName = None, headerList = None):
+        try:
+            if csvFileName is None:
+                csvFileName = self.__csvFileName
+            if headerList is None:
+                headerList = self.__paramNames
+
+            with open(csvFileName, 'r') as csvFile:
+                reader = csv.DictReader(csvFile, delimiter=';')
+                for row in reader:
+                    newData = list()
+                    for columnName, value in row.items() :
+                        if columnName == labelColumnName:
+                            self.__label.append(value)
+                        else:
+                            newData.append(value)
+                self.__data.append(newData)
+        except Exception as e:
+            return e
