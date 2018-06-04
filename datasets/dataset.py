faa6473c11cfbc2fec83578a08fb75f2c654acfd
Henrique Borges <nervmaster@localhost.localdomain>
2018-06-04 20:15:39 -0300

    Common methods of dataset almost over
    TODO:
    First file parser
    Create a requirements txt file for PIP dependencies
    Extract the image params

--- datasets/dataset.py
+++ datasets/dataset.py
@@ -1,24 +1,19 @@
 from abc import ABC, abstractmethod
+import csv
 
 class Dataset(ABC):
-    self.__data 
-    self.__label 
-    self.__paramNames 
-    self.__csvFileName 
-
     def __init__(self, paramNames, csvFileName = None):
         # ParamNames must be passed as arg for the desired parameters
         # Checks if has a csvFileName
         # if not parse files
         # if yes load the csv
-        pass
+        self.__data = [] 
+        self.__label = [] 
+        self.__paramNames = paramNames 
+        self.__csvFileName = csvFileName
 
 
     def getData(self):
-        return self.__data
-        
-
-    def getDataWithLabel(self):
         return self.__data, self.__label
 
     @abstractmethod
@@ -37,32 +32,40 @@ class Dataset(ABC):
             return e
         
         writer = csv.writer(arq, delimiter=';')
-        writer.writerow(header)
+        writer.writerow(headerList)
         arq.flush()
 
         return arq, writer
 
-    @abstractmethod
-    def writeCsv(self, csvFileName = None, headerList = None):
-        pass
+    def writeCsv(self, labelColumnName = 'label' ,csvFileName = None, headerList = None):
+        if csvFileName is None:
+            csvFileName = self.__csvFileName
+        if headerList is None:
+            headerList = self.__paramNames
+        
+        with csv.writer(csvFileName, delimiter=';') as writer:
+            writer.writerow([labelColumnName].extend(self.__paramNames))
+            for label, data in zip(self.__label, self.__data):
+                row = []
+                row.append(label)
+                for elem in data:
+                    row.append(elem)
+                writer.writerow(row)
         
     
-    def readCsv(self, labelColumnName, csvFileName = None, headerList = None):
-        try:
-            if csvFileName is None:
-                csvFileName = self.__csvFileName
-            if headerList is None:
-                headerList = self.__paramNames
+    def readCsv(self, labelColumnName = 'label', csvFileName = None, headerList = None):
+        if csvFileName is None:
+            csvFileName = self.__csvFileName
+        if headerList is None:
+            headerList = self.__paramNames
 
-            with open(csvFileName, 'r') as csvFile:
-                reader = csv.DictReader(csvFile, delimiter=';')
-                for row in reader:
-                    newData = list()
-                    for columnName, value in row.items() :
-                        if columnName == labelColumnName:
-                            self.__label.append(value)
-                        else:
-                            newData.append(value)
-                self.__data.append(newData)
-        except Exception as e:
-            return e
+        with open(csvFileName, 'r') as csvFile:
+            reader = csv.DictReader(csvFile, delimiter=';')
+            for row in reader:
+                newData = list()
+                for columnName, value in row.items() :
+                    if columnName == labelColumnName:
+                        self.__label.append(value)
+                    else:
+                        newData.append(value)
+            self.__data.append(newData)
